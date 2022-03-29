a1aad66a026d0bdaac125bcc31f35bc1aea334a4
Piotr Bator <82182989+piotek8@users.noreply.github.com>
2022-03-29 18:36:18 +0000

    new lines

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-from time import sleep
+""" from time import sleep
+
+for my_box in [10,9,8,7,6,5,4,3,2,1,0]:
+  print(my_box) 
+  if my_box == 2: 
+    sleep(1)
+  if my_box == 1: 
+    sleep(1)
+  if my_box == 0: 
+    sleep(1)  
+  sleep(1)
+
+print ('boom')"""
+
+
+ 
+'''
+//////////////////////////////////
+
 
 print ('1')
 sleep (0.1)
@@ -20,4 +38,141 @@ print ('9')
 sleep (0.1)
 print ('10')
 sleep (0.025)
-print ('START')
+print ('START') 
+
+
+//////////////////////////////////
+
+ '''
+
+'''
+
+from time import sleep
+
+for my_box in [10,9,8,7,6,5,4,3,2,1,0]:
+  print(my_box)  
+
+  if my_box == 2:
+    sleep (1.5)
+
+print ('boooom')
+
+//////////////////////////////////
+'''
+
+'''
+x = input ('Podaj imie : ')
+print ('Podane imie to')
+print (x)
+ 
+ 
+NIE DZIALA NIE DZIALA NIE DZIALA
+  
+Player_choice1 = input ('Player 1 give your choice: ')
+Player_choice2 = input ('Player 2 give your choice: ')
+
+
+if Player_choice1 == 'paper' :   
+  if Player_choice2 == 'stone':
+    print ('Player 1 : wins')  
+
+
+
+
+if Player_choice1 == 'paper':      
+  if Player_choice2 == 'scissors'
+    print ('Player 1 : loses')
+    print ('Player 2 : wins')
+
+if Player_choice1 == 'paper':      
+  if Player_choice2 == 'paper'
+    print ('Player 1 : draw')
+    print ('Player 2 : draw')
+
+
+
+if Player_choice1 == 'stone':      
+  if Player_choice2 == 'paper'
+    print ('Player 1 : loses')
+    print ('Player 2 : wins')
+
+if Player_choice1 == 'stone':      
+  if Player_choice2 == 'scissors'
+     print ('Player 1 : wins')
+     print ('Player 2 : loses')
+
+if Player_choice1 == 'stone':      
+  if Player_choice2 == 'stone'
+     print ('Player 1 : draw')
+     print ('Player 2 : draw')
+
+
+
+if Player_choice1 == 'scissors':      
+  if Player_choice2 == 'stone'
+     print ('Player 1 : loses')
+     print ('Player 2 : wins')
+
+if Player_choice1 == 'scissors':      
+  if Player_choice2 == 'paper'
+     print ('Player 1 : wins')
+     print ('Player 2 : loses')
+
+if Player_choice1 == 'scissors':      
+  if Player_choice2 == 'scissors'
+     print ('Player 1 : draw')
+     print ('Player 2 : draw')
+
+
+
+Player_choice1 = input ('Player 1 give your choice: ')
+Player_choice2 = input ('Player 2 give your choice: ')
+
+if Player_choice1 == 'scissors' and Player_choice2 == 'scissors':
+     print ('Player 1 : draw Player 2 : draw ')
+     print ('Player 2 : draw')
+
+
+
+
+NIE DZIALA NIE DZIALA NIE DZIALA
+
+
+  pojemnik = 4
+  
+  if pojemnik == 4:
+      print ('dziala dla 4')
+  
+  if pojemnik == 5:
+      print ('dziala dla 5')
+
+'''
+
+Player_choice1 = input ('Player 1 give your choice: ')
+Player_choice2 = input ('Player 2 give your choice: ')
+
+if player_choice1 == 'paper' :
+  if player_choice2 == 'stone' :
+    print('Player1 win')
+
+elif player_choice1 == 'stone':
+  if player_choice2 == 'scissors':
+    print('Player1 win')
+
+elif player_choice1 == 'scissors' :
+  if player_choice2 == 'paper':
+    print('Player1 win')
+
+else: 
+  print('Player2 win')
+
+
+
+
+
+
+
+
+
+
+
